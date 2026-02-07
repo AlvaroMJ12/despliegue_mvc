0fc958a6a7d1c6a8a30e0d1d32886f8aef899648
AlvaroMJ12 <alvaromj1201@gmail.com>
2026-02-07 16:13:37 +0100

    Enable error reporting in index.php
    Added error reporting settings for better debugging.

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 // Este será el punto de entrada siempre en nuestra aplicación web
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //Incluimos el FrontController que es el controlador de inicio de la aplicación
 require 'libs/FrontController.php';
 
 //Lo iniciamos con su método estático main.
 FrontController::main();
+
 ?>
