caa23551f2e42d2528ebf6a257f35ee80f82778d
AlvaroMJ12 <alvaromj1201@gmail.com>
2026-02-07 14:12:23 +0100

    Update SPDO.php

--- libs/SPDO.php
+++ libs/SPDO.php
@@ -1,30 +1,35 @@
 <?php
 // Clase que hereda de PDO y que instancia la conexión a la BD con PDO
 // Utiliza el patrón Singleton para que en la aplicación web únicamente
-// exista una instancia de dicha conexión a la BD
-class SPDO extends PDO
+// existe una instancia de dicha conexión a la BD
+class SPDO extiende PDO
 {
     // Atributo que es la referencia a la instancia del objeto PDO con la conexión
-    private static $instance = null;
+    privado estático $instancia = nulo;
 
-    // Constructor que usa el patrón Singleton de Config para obtener la configuración
-    // y llama al constructor de la clase padre, que es PDO, con los parámetros de Config
-    public function __construct()
+    // Constructor que obtiene la configuración de las variables de entorno de Railway
+    público función __construir()
     {
-        $config = Config::singleton();
-        parent::__construct(
-            'mysql:host=' . $config->get('dbhost') . ';dbname=' . $config->get('dbname'),
-            $config->get('dbuser'), $config->get('dbpass')
-        );
+        // En Railway, usamos getenv() para leer las variables que configuramos en el panel
+        $host = obtenerenv('MYSQLHOST');
+        $db   = obtenerenv('MYSQLDATABASE');
+        $user = obtenerenv('MYSQLUSER');
+        $pass = obtenerenv('MYSQLPASSWORD');
+        $port = obtenerenv('MYSQLPORT');
+
+        // Nota técnica: En el DSN de PDO para MySQL se usa 'dbname', no 'nombredb'
+        $dsn = "mysql:host={$host};port={$port};dbname={$db};charset=utf8";
+
+        padre::__construir($dsn, $user, $pass);
     }
 
     // Método estático para el patrón Singleton que devuelve la instancia de SPDO
-    public static function singleton()
+    público estático función singleton()
     {
-        if (self::$instance == null) {
-            self::$instance = new self();
+        si (mismo::$instancia == nulo) {
+            mismo::$instancia = nuevo mismo();
         }
-        return self::$instance;
+        regresar mismo::$instancia;
     }
 }
 ?>
